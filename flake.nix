1f2dd4a0fe491d01dfa6b5260c67a76f0bfc9f68
PerchunPak <git@perchun.it>
2024-11-17 16:23:38 +0100

    Fix GitHub CI

--- flake.nix
+++ flake.nix
@@ -2,6 +2,10 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    nix-github-actions = {
+      url = "github:nix-community/nix-github-actions";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
@@ -9,6 +13,7 @@
       self,
       nixpkgs,
       flake-utils,
+      nix-github-actions,
       ...
     }:
     flake-utils.lib.eachDefaultSystem (
@@ -34,15 +39,31 @@
 
         packages = {
           default = pkgs.callPackage ./nix/package.nix { };
-          nixpkgs-updaters-library = outputs.packages.${system}.default;
-          nupd = outputs.packages.${system}.default;
+          nixpkgs-updaters-library = self.packages.${system}.default;
+          nupd = self.packages.${system}.default;
         };
 
         devShell = pkgs.mkShell {
-          inputsFrom = [ outputs.packages.${system}.default ];
+          inputsFrom = [ self.packages.${system}.default ];
 
           PYTHONPATH = ".";
         };
       }
-    );
+    )
+    // {
+      githubActions = nix-github-actions.lib.mkGithubMatrix {
+        checks = builtins.listToAttrs (
+          map
+            (system: {
+              name = system;
+              value.default = self.packages.${system}.default;
+            })
+            [
+              "x86_64-linux"
+              "x86_64-darwin"
+              "aarch64-darwin"
+            ]
+        );
+      };
+    };
 }
