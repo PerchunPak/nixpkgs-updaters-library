95326fe790a2ec29fef34473951897e062421042
PerchunPak <git@perchun.it>
2024-11-26 22:22:19 +0100

    Add some general description to models

--- nupd/models.py
+++ nupd/models.py
@@ -7,6 +7,8 @@ from attrs import define
 
 @define(frozen=True)
 class EntryInfo(abc.ABC):
+    """A minimal amount of information that is only enough to prefetch the entry."""
+
     @abc.abstractmethod
     async def fetch(self) -> Entry:
         raise NotImplementedError
@@ -14,4 +16,6 @@ class EntryInfo(abc.ABC):
 
 @define(frozen=True)
 class Entry(abc.ABC):
+    """All information about the entry, that we need to generate Nix code."""
+
     info: EntryInfo
