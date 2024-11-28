70cb2021373f9be1e0881e3b158be45b7ace4e68
PerchunPak <git@perchun.it>
2024-11-28 21:19:55 +0100

    Add a test for `utils.chunks`

---
+++ tests/test_utils.py
@@ -0,0 +1,15 @@
+import pytest
+
+from nupd.utils import chunks
+
+
+@pytest.mark.parametrize(
+    "n,result",
+    [
+        (1, [[1], [2], [3], [4], [5]]),
+        (3, [[1, 2, 3], [4, 5]]),
+        (5, [[1, 2, 3, 4, 5]]),
+    ],
+)
+def test_utils_chunks(n: int, result: list[list[int]]) -> None:
+    assert list(chunks([1, 2, 3, 4, 5], n)) == result
