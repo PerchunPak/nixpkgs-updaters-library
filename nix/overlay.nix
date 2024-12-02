7710cfbdbfbf5d97af8156d99d7c8508ae5557f2
PerchunPak <git@perchun.it>
2024-12-02 13:50:41 +0100

    Update nixpkgs

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -5,12 +5,4 @@ final: prev: {
       nbdb = final.callPackage ./python-nbdb.nix { pythonPackages = pfinal; };
     })
   ];
-
-  basedpyright = prev.basedpyright.overrideAttrs (oa: rec {
-    version = "1.22.0";
-    src = oa.src.override {
-      rev = "refs/tags/v${version}";
-      hash = "sha256-/I8KCQnjFbE64h2rQuLV31IsVTQhuDxiobQwtx0HRPM=";
-    };
-  });
 }
