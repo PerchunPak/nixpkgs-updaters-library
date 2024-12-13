19f10bd392f6cec92bfd9b02cca8dafaa6658e43
PerchunPak <git@perchun.it>
2024-12-13 02:47:35 +0100

    Apply my small patch to python3Packages.inject
    To allow lambdas as binders

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -1,8 +1,16 @@
 final: prev: {
   pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
     (pfinal: pprev: {
-      # I will need this eventually
       nbdb = final.callPackage ./python-nbdb.nix { pythonPackages = pfinal; };
+
+      inject = pprev.inject.overrideAttrs (old: {
+        patches = (old.patches or [ ]) ++ [
+          (final.fetchpatch2 {
+            url = "https://patch-diff.githubusercontent.com/raw/ivankorobkov/python-inject/pull/110.diff";
+            hash = "sha256-T98p9zK+U14pR5ybzIxl3jrn78jmwCJNQWg+HcyO4dc=";
+          })
+        ];
+      });
     })
   ];
 }
